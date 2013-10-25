9ec00217c0a1fb9b1638c9cf796bef4cdf4ee45c
Jose Buitron <jose.buitron@gmail.com>
2013-10-25 18:36:45 +1100

    Stop sending data when game is over

--- car-game/src/cargame/sync/GameSync.java
+++ car-game/src/cargame/sync/GameSync.java
@@ -70,7 +70,7 @@ public class GameSync extends Thread implements Client {
 				if(CarGame.getInstance().checkStatus(CarGame.STATUS_PLAYING) && lost_packets >= PERMITED_MESSAGE_LOST){
 					CarGame.getInstance().setConnectionLost(true);
 				}
-				if(this.peerAddress != null){
+				if(this.peerAddress != null && CarGame.getInstance().checkStatus(CarGame.STATUS_PLAYING)){
 					sendData();
 				}
 			}
